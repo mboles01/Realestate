9169219783cc1b337a3c6f697b3cff262d885fd6
Michael Boles <michaelboles@Michaels-MacBook-Pro.local>
2019-06-24 22:01:29 -0700

    Worked on multiple linear regression

--- fitdata.py
+++ fitdata.py
@@ -7,8 +7,8 @@ Created on Mon Jun 24 16:31:23 2019
 
 # set up working directory
 import os
-#os.chdir('/Users/michaelboles/Michael/Coding/2019/Realestate') # Mac
-os.chdir('C:\\Users\\bolesmi\\Lam\\Coding\\Python\\2019\\Realestate') # PC
+os.chdir('/Users/michaelboles/Michael/Coding/2019/Realestate') # Mac
+#os.chdir('C:\\Users\\bolesmi\\Lam\\Coding\\Python\\2019\\Realestate') # PC
 
 # import master dataset    
 import pandas as pd
@@ -22,4 +22,13 @@ y = data_all['Price'].values
 from sklearn.model_selection import train_test_split
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 1)
 
+# fit data with multiple linear regression
 from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(x_train, y_train)
+
+# predict test set
+y_pred = regressor.predict(x_test)
+
+# build optimal model using backward elimination
+import statsmodels.formula.api as sm
