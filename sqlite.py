5b4731d9155582209d8c7aed07a4494661bc8e20
Michael Boles <michaeladamboles@gmail.com>
2019-06-27 09:47:17 -0700

    worked on SQL queries

--- sqlite.py
+++ sqlite.py
@@ -7,38 +7,44 @@ Created on Sun Jun 23 12:38:50 2019
 """
 
 # set up working directory
-import  os
-os.chdir('/Users/michaelboles/Michael/Coding/2019/Realestate') # Mac
-#os.chdir('C:\\Users\\bolesmi\\Lam\\Coding\\Python\\2019\\Realestate') # PC
+import os
+#os.chdir('/Users/michaelboles/Michael/Coding/2019/Realestate') # Mac
+os.chdir('C:\\Users\\bolesmi\\Lam\\Coding\\Python\\2019\\Realestate') # PC
 
 # import data
 import pandas as pd
-data = pd.read_csv('data_clean.csv')
+data_temp = pd.read_csv('data_clean.csv')
 
-# drop second column
-data2 = data.drop(['Unnamed: 0'], axis = 1)
+# drop second column - extra index
+data = data_temp.drop(['Unnamed: 0'], axis = 1)
 
-# sqlite 3
-import sqlite3 
-conn = sqlite3.connect('data.db')
-data.to_sql(data, conn, if_exists='append', index=False)
+# remind myself what the column names are
+data.columns
+
+# create in-memory sqlite database, add dataframe
+from sqlalchemy import create_engine
+engine = create_engine('sqlite://', echo = False)
+data.to_sql('Realestate', con=engine)
+
+# query database
+engine.execute("SELECT * FROM Realestate").fetchall() # gets everything
+engine.execute('SELECT * FROM Realestate WHERE Zip = 94618').fetchall() # matches a zipcode
+
+# create a list from sql query 
+# returns list of rowproxy objects, omits column names - why is this so hard
+rockridge = engine.execute('SELECT * FROM Realestate WHERE Zip = 94618').fetchall() # matches a zipcode
+
+### switch to pandas? ###
+
+# do analogous queries in pandas
+rockridge = data.loc[data['Zip'] == 94618]
+
+# set latitude, longitude window for bay, sf, south bay, east bay
 
 
-import csv, sqlite3
-conn_memory = sqlite3.connect(":memory:")
-cur = conn_memory.cursor()
-cur.execute("CREATE TABLE data ('Address','City','State','Zip','Price','Beds','Baths','Home size','Lot size','Latitude','Longitude')") 
 
-with open('data_clean.csv','rt') as fin: 
-    # csv.DictReader uses first line in file for column headings by default
-    dr = csv.DictReader(fin) # comma is default delimiter
-    to_db = [(i['Address'], i['City']) for i in dr]
 
-cur.executemany("INSERT INTO data (Address, City) VALUES (?, ?);", to_db)
-conn.commit()
-conn.close()
 
-cur.execute("SELECT * FROM data")
-rows = cur.fetchall()
-for row in rows:
-    print(row)
+
+
+
