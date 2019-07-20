5d8990432ba92752413035f4dc3289f1880d569a
Michael Boles <michaeladamboles@gmail.com>
2019-07-19 19:28:26 -0700

    Updated pairplots

--- fitdata.py
+++ fitdata.py
@@ -15,10 +15,17 @@ import pandas as pd
 from scipy import stats
 import numpy as np
 import statsmodels.formula.api as smf
-import seaborn as sns
 
 # import master dataset    
 data_bay_withtimes = pd.read_csv('./data/data_bay_withtimes.csv')
+
+# count number, frequency of unique zipcodes
+cityzips = data_bay_withtimes[['City','Zip']]
+numzips = data_bay_withtimes['Zip'].nunique()
+topzips = data_bay_withtimes['Zip'].value_counts().to_frame()
+topzips['Count'] = topzips['Zip']
+topzips['Zip'] = topzips.index
+topzips_city = topzips.merge(cityzips, on='Zip', how='inner')
 
 # import ancillary dataset
 data_schools = pd.read_csv('./School data/school_quality_bay.csv')
@@ -43,38 +50,10 @@ data_all_temp6 = data_all_temp5[data_all_temp5['Lot_size'] < 20000]
 data_all_temp7 = data_all_temp6[data_all_temp6['Price'] < 5000000]
 data_all_temp8 = data_all_temp7[data_all_temp7['Beds'] < 6]
 data_all_temp9 = data_all_temp8[data_all_temp8['Baths'] < 6]
+data_all = data_all_temp9
 
-# rescale for pairplot
-data_all_temp9['Price'] = data_all_temp9['Price']/1000000
-
-# rename series for pairplot
-data_all = data_all_temp9.rename(columns = {'Home_size': 'Home size (sqft)', 
-                                            'Lot_size': 'Lot size (sqft)',
-                                            'Price': 'Price ($M)'})
-
-# create pairplot with only price as y-axis
-sns.set(style="ticks", color_codes=True)
-g = sns.pairplot(data_all, 
-             plot_kws=dict(line_kws=dict(color = '#3148a3'), 
-                           scatter_kws=dict(facecolor = '#5797ff', edgecolor = '#3148a3', linewidth = 0.33, alpha = 0.05)),
-             x_vars = ['Beds', 'Baths', 'Home size (sqft)', 'Lot size (sqft)'], 
-                       #'Min_commute', 'School_score'], #'SF_time', 'PA_time', 
-             y_vars = 'Price ($M)', kind = 'reg')
-
-g.savefig('pairplot_price_all.jpg', dpi=500)
-
-# create overall pairplot
-sns.pairplot(data_temp3, diag_kind = 'kde', kind = 'reg',
-             plot_kws = dict(scatter_kws = dict(edgecolor = 'w')))
-
-
-# count number, frequency of unique zipcodes
-cityzips_temp = data_bay_withtimes[['City','Zip']]
-numzips = data_bay_withtimes['Zip'].nunique()
-topzips = data_bay_withtimes['Zip'].value_counts().to_frame()
-topzips['Count'] = topzips['Zip']
-topzips['Zip'] = topzips.index
-topzips_city = topzips.merge(cityzips, on='Zip', how='inner')
+# check for multicollinearity
+correlations = data_all.corr()
 
 # option to select one zipcode
 zipcode = 94401
@@ -90,22 +69,8 @@ data_subset = data_all[data_all['Zip'] == zipcode]
 #data_to_fit = data_temp4[(z < 3).all(axis=1)]
 ##data_to_fit = data_temp4
 
-# create subset pairplot
-sns.set(style="ticks", color_codes=True)
-sns.pairplot(data_subset.drop('Zip', axis = 1), diag_kind = 'kde', kind = 'reg',
-             plot_kws = dict(scatter_kws = dict(edgecolor = 'w')))
-
-# create pairplot with only price as y-axis
-sns.set(style="ticks", color_codes=True)
-g = sns.pairplot(data_subset, 
-             plot_kws=dict(line_kws=dict(color = '#3148a3'), scatter_kws=dict(facecolor = '#5797ff', edgecolor = 'w', linewidth = 0.33)),
-             x_vars = ['Beds', 'Baths', 'Home_size', 'Lot_size'], 
-                       #'Min_commute', 'School_score'], #'SF_time', 'PA_time', 
-             y_vars = 'Price', kind = 'reg')
-g.savefig('pairplot_price_subset_' + str(zipcode) + '.jpg', dpi=500)
-
 # query count, mean, stdev etc. of selected data
-data_to_fit.describe()
+data_all.describe()
 
 
 ### BUILD MODEL ###
@@ -113,104 +78,45 @@ data_to_fit.describe()
 
 
 # build model from data
-formula = 'Price ~ Home_size + Lot_size + Beds + Baths + Min_commute + SF_time + PA_time'
-regressor = smf.ols(formula, data = data_temp3).fit()
 
-
-
-
-
-
-
-
-# count number, frequency of unique zipcodes
-data_bay['Zip'].nunique() 
-data_bay['Zip'].value_counts()
-
-# remove zeros, NaNs, index column
-data_temp2 = data_all.dropna()
-data_clean = data_temp2[(data_temp2 != 0).all(1)]
-
-
-# select only columns for fitting
-data_to_fit = data_bay[['Beds', 'Baths', 'Home size', 'Lot size', 'Price']]
-
-# get data summary
-data_to_fit.describe()
-
-# find and remove outliers
-z = np.abs(stats.zscore(data_to_fit[['Beds', 'Baths', 'Home size', 'Lot size', 'Price']]))
-np.where(z > 3)
-data_to_fit.iloc[4422]
-
-data_no_outliers = data_to_fit[(z < 3).all(axis=1)]
+formula_1 = 'Price ~ Home_size + Lot_size + Beds + Baths + Min_commute + School_score'
+formula_2 = 'Price ~ Home_size + Lot_size + Beds + Min_commute + School_score'
+formula_3 = 'Price ~ Home_size + Lot_size + Min_commute + School_score'
+regressor = smf.ols(formula_1, data = data_all).fit()
+regressor.summary()
+summary = regressor.summary()
+summary_text = summary.as_text()
 
 # try same for only one zipcode
-data_subset = data_clean.loc[data_clean['Zip'] == 95129]
-data_subset_fit = data_subset[['Beds', 'Baths', 'Home size', 'Lot size', 'Price']]
+data_subset = data_all.loc[data_all['Zip'] == 95126]
+formula_subset_1 = 'Price ~ Home_size + Lot_size + Beds + Baths + Min_commute + School_score'
+formula_subset_2 = 'Price ~ Home_size + Lot_size + Baths'
+regressor = smf.ols(formula_subset_2, data = data_subset).fit()
+summary_subset = regressor.summary()
+summary_subset_text = summary_subset.as_text()
 
+# get variance inflation factor
+from statsmodels.stats.outliers_influence import variance_inflation_factor
+variables = regressor.model.exog
+vif = [variance_inflation_factor(variables, i) for i in range(variables.shape[1])]
 
-### FITTING ###
-
-# assign x and y variables
-x = data_no_outliers[['Home size', 'Lot size', 'Beds', 'Baths']].values
-y = data_no_outliers['Price'].values
-
-# add column of ones to x
-x = np.append(arr = np.ones((x.shape[0], 1)).astype(int), values = x, axis = 1)
-
-
-### insert additional features ###
-data_bay_withtimes = pd.read_csv('./data/data_bay_withtimes.csv')
-data_bay_withtimes.columns = data_bay_withtimes.columns.str.replace(' ', '_')
-data_temp1 = data_bay_withtimes.dropna()
-data_temp2 = data_temp1
-#data_temp2 = data_temp1[data_temp1['Zip'].isin([94618])]
-data_temp3 = data_temp2[(data_temp2 != 0).all(1)]
-data_temp3['Min_commute'] = data_temp3[['SF_time','PA_time']].min(axis=1)
-
-z = np.abs(stats.zscore(data_temp3[['Beds', 'Baths', 'Home_size', 'Lot_size', 
-                                  'Price', 'SF_time', 'PA_time', 'Min_commute']]))
-data_to_fit = data_temp3[(z < 3).all(axis=1)]
-data_to_fit.describe()
-data_temp3.describe()
-
-# build model from data
-import statsmodels.formula.api as smf
-
-formula = ='Price ~ Home_size + Lot_size + Beds + Baths + Min_commute + SF_time + PA_time'
-regressor = smf.ols(formula, data = data_temp3).fit()
+### sklearn ###
 
 
 
-regressor.summary()
+# split data into test and training sets
+from sklearn.model_selection import train_test_split
 
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 1)
 
+# fit data with multiple linear regression
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(x_train, y_train)
 
+# predict test set
+y_pred = regressor.predict(x_test)
 
-## split data into test and training sets
-#from sklearn.model_selection import train_test_split
-#x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 1)
-#
-## fit data with multiple linear regression
-#from sklearn.linear_model import LinearRegression
-#regressor = LinearRegression()
-#regressor.fit(x_train, y_train)
-#
-## predict test set
-#y_pred = regressor.predict(x_test)
-
-# build model from all data
-import statsmodels.formula.api as smf
-data_bay2 = data_bay.rename(index=str, columns={"Home size": "Homesize", "Lot size": "Lotsize"})
-regressor_all = smf.ols(formula='Price ~ Homesize + Lotsize + Beds + Baths', data=data_bay2).fit()
-regressor_all.summary()
-
-# build model from data subset
-import statsmodels.formula.api as smf
-data_subset2 = data_subset.rename(index=str, columns={"Home size": "Homesize", "Lot size": "Lotsize"})
-regressor_subset = smf.ols(formula='Price ~ Homesize + Lotsize + Beds + Baths', data=data_subset2).fit()
-regressor_subset.summary()
 
 
 
--- plotpairplots.py
+++ plotpairplots.py
@@ -13,6 +13,7 @@ os.chdir('C:\\Users\\bolesmi\\Lam\\Coding\\Python\\2019\\Realestate') # PC
 # import packages
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 # import master dataset    
 data_bay_withtimes = pd.read_csv('./data/data_bay_withtimes.csv')
@@ -45,12 +46,20 @@ data_all_temp9 = data_all_temp8[data_all_temp8['Baths'] <= 5]
 data_all_temp9['Price'] = data_all_temp9['Price']/1000000
 
 # rename series for pairplot
-data_all.columns
 data_all = data_all_temp9.rename(columns = {'Home_size': 'Home size (sqft)', 
                                             'Lot_size': 'Lot size (sqft)',
                                             'Min_commute': 'Commute time (mins)',
                                             'School_score': 'School score (%)',
                                             'Price': 'Price ($M)'})
+
+# get correlations, round
+corr_beds_price = round(data_all['Beds'].corr(data_all['Price ($M)']),2)
+corr_baths_price = round(data_all['Baths'].corr(data_all['Price ($M)']),2)
+corr_home_price = round(data_all['Home size (sqft)'].corr(data_all['Price ($M)']),2)
+corr_lot_price = round(data_all['Lot size (sqft)'].corr(data_all['Price ($M)']),2)
+corr_commute_price = round(data_all['Commute time (mins)'].corr(data_all['Price ($M)']),2)
+corr_school_price = round(data_all['School score (%)'].corr(data_all['Price ($M)']),2)
+
 
 # create pairplot with only price as y-axis
 
@@ -62,8 +71,13 @@ g = sns.pairplot(data_all,
                                             linewidth = 0.33, alpha = 0.1, s = 20)),
              x_vars = ['Beds', 'Baths', 'Home size (sqft)', 'Lot size (sqft)'], 
              y_vars = 'Price ($M)', kind = 'reg')
+spacing = 0.235
+g.fig.text(0.08, 0.825, r'$R^{2}$ = ' + str(corr_beds_price), fontsize=10)
+g.fig.text(0.08 + spacing, 0.825, r'$R^{2}$ = ' + str(corr_baths_price), fontsize=10)
+g.fig.text(0.08 + 2*spacing, 0.825, r'$R^{2}$ = ' + str(corr_home_price), fontsize=10)
+g.fig.text(0.08 + 3*spacing, 0.825, r'$R^{2}$ = ' + '0.30', fontsize=10)
 
-g.savefig('pairplot_price_home.jpg', dpi=500)
+g.savefig('pairplot_price_home.jpg', dpi=600)
 
 # home + surroundings data
 g = sns.pairplot(data_all, 
@@ -73,19 +87,32 @@ g = sns.pairplot(data_all,
              x_vars = ['Beds', 'Baths', 'Home size (sqft)', 'Lot size (sqft)', 
                       'Commute time (mins)', 'School score (%)'], 
              y_vars = 'Price ($M)', kind = 'reg')
+spacing2 = (4/6)*spacing + 0.0025
+g.fig.text(0.06, 0.825, r'$R^{2}$ = ' + str(corr_beds_price), fontsize=10)
+g.fig.text(0.06 + spacing2, 0.825, r'$R^{2}$ = ' + str(corr_baths_price), fontsize=10)
+g.fig.text(0.06 + 2*spacing2, 0.825, r'$R^{2}$ = ' + str(corr_home_price), fontsize=10)
+g.fig.text(0.06 + 3*spacing2, 0.825, r'$R^{2}$ = ' + '0.30', fontsize=10)
+g.fig.text(0.06 + 4*spacing2, 0.825, r'$R^{2}$ = ' + str(corr_commute_price), fontsize=10)
+g.fig.text(0.06 + 5*spacing2, 0.825, r'$R^{2}$ = ' + str(corr_school_price), fontsize=10)
 
-g.savefig('pairplot_price_all.jpg', dpi=500)
+g.savefig('pairplot_price_all.jpg', dpi=600)
 
 # create overall pairplot
+
+def corrfunc(x, y, **kws):
+    r = x.corr(y)
+    ax = plt.gca()
+    ax.annotate(r'$R^{2}$ = ' + str(round(r, 2)), xy=(.1, .9), xycoords=ax.transAxes, backgroundcolor = 'white')
+
 g = sns.pairplot(data_all, diag_kind = 'kde', kind = 'reg',
              plot_kws=dict(line_kws=dict(color = '#3148a3'), 
                            scatter_kws=dict(facecolor = '#5797ff', edgecolor = '#3148a3', 
                                             linewidth = 0.33, alpha = 0.1, s = 20)),
              x_vars = ['Beds', 'Baths', 'Home size (sqft)', 'Lot size (sqft)', 
-                      'Commute time (mins)', 'School score (%)', 'Price ($M)'], 
+                      'Commute time (mins)', 'School score (%)'], 
              y_vars = ['Beds', 'Baths', 'Home size (sqft)', 'Lot size (sqft)', 
-                       'Commute time (mins)', 'School score (%)', 'Price ($M)'])
-
+                       'Commute time (mins)', 'School score (%)'])
+g.map_lower(corrfunc)
 g.savefig('pairplot_data_all_price.jpg', dpi=300)
 
 
@@ -112,10 +139,17 @@ data_subset = data_all[data_all['Zip'] == zipcode]
 #data_to_fit = data_temp4[(z < 3).all(axis=1)]
 ##data_to_fit = data_temp4
 
-# create subset pairplot
-sns.set(style="ticks", color_codes=True)
-sns.pairplot(data_subset.drop('Zip', axis = 1), diag_kind = 'kde', kind = 'reg',
-             plot_kws = dict(scatter_kws = dict(edgecolor = 'w')))
+# get correlations within data subset
+corr_beds_price_subset = round(data_subset['Beds'].corr(data_subset['Price ($M)']),2)
+corr_baths_price_subset = round(data_subset['Baths'].corr(data_subset['Price ($M)']),2)
+corr_home_price_subset = round(data_subset['Home size (sqft)'].corr(data_subset['Price ($M)']),2)
+corr_lot_price_subset = round(data_subset['Lot size (sqft)'].corr(data_subset['Price ($M)']),2)
+
+
+## create subset pairplot
+#sns.set(style="ticks", color_codes=True)
+#sns.pairplot(data_subset.drop('Zip', axis = 1), diag_kind = 'kde', kind = 'reg',
+#             plot_kws = dict(scatter_kws = dict(edgecolor = 'w')))
 
 # create pairplot with only price as y-axis
 sns.set(style="ticks", color_codes=True)
@@ -124,8 +158,12 @@ g = sns.pairplot(data_subset,
                            scatter_kws=dict(facecolor = '#5797ff', edgecolor = '#3148a3', 
                                             linewidth = 0.33, alpha = 1, s = 20)),
              x_vars = ['Beds', 'Baths', 'Home size (sqft)', 'Lot size (sqft)'], 
-                       #'Min_commute', 'School_score'], #'SF_time', 'PA_time', 
              y_vars = 'Price ($M)', kind = 'reg')
+spacing = 0.235
+g.fig.text(0.08, 0.825, r'$R^{2}$ = ' + str(corr_beds_price_subset), fontsize=10)
+g.fig.text(0.08 + spacing, 0.825, r'$R^{2}$ = ' + str(corr_baths_price_subset), fontsize=10)
+g.fig.text(0.08 + 2*spacing, 0.825, r'$R^{2}$ = ' + '0.90', fontsize=10)
+g.fig.text(0.08 + 3*spacing, 0.825, r'$R^{2}$ = ' + str(corr_lot_price_subset), fontsize=10)
 
-g.savefig('pairplot_price_subset_' + str(zipcode) + '.jpg', dpi=500)
+g.savefig('pairplot_price_subset_' + str(zipcode) + '.jpg', dpi=900)
 
