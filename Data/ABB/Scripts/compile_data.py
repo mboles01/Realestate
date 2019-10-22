c52361960ca85a13f0e37e9c64018a3462183eef
Michael Boles <michaeladamboles@gmail.com>
2019-10-22 15:57:08 -0700

    Updated data

--- Data/ABB/Scripts/compile_data.py
+++ Data/ABB/Scripts/compile_data.py
@@ -8,15 +8,15 @@ Created on Sun Oct 20 19:25:15 2019
 
 # set up working directory
 import os
-os.chdir('/Users/michaelboles/Michael/Coding/2019/Realestate/Data/ABB/Data/San Francisco') # Mac
-#os.chdir('C:\\Users\\bolesmi\\Lam\\Coding\\Python\\2019\\Realestate') # PC
+#os.chdir('/Users/michaelboles/Michael/Coding/2019/Realestate/Data/ABB/Data/San Francisco') # Mac
+os.chdir('C:\\Users\\bolesmi\\Lam\\Coding\\Python\\2019\\Realestate\\Data\ABB') # PC
 
 # import packages
 import pandas as pd
 import numpy as np
 
-# import full dataset 
-data_all_raw = pd.read_csv('./listings 2.csv')
+# import SF listings dataset 
+data_all_raw = pd.read_csv('.\Data\San_Francisco\listings 2.csv')
 
 # get column names 
 data_colnames = list(data_all_raw)
@@ -30,7 +30,8 @@ data_all = data_all_raw[['id', 'name', 'host_name',
                          'property_type', 'room_type',
                          'guests_included', 'extra_people', 'minimum_nights',
                          'availability_30', 'availability_90',
-                         'number_of_reviews', 'review_scores_rating', 'last_review',
+                         'number_of_reviews', 'review_scores_rating', 'last_review', 
+                         'reviews_per_month',
                          'price', 'cleaning_fee', 'security_deposit']].copy()
 
 # CLEAN DATA
@@ -56,9 +57,10 @@ g = sns.pairplot(data_1, diag_kind = 'kde', kind = 'reg',
              plot_kws=dict(line_kws=dict(color = '#3148a3'), 
                            scatter_kws=dict(facecolor = '#5797ff', edgecolor = '#3148a3', 
                                             linewidth = 0.33, alpha = 0.1, s = 20)),
-             x_vars = ['bedrooms', 'bathrooms', 'price'], 
-             y_vars = ['bedrooms', 'bathrooms', 'price'])
+             x_vars = ['bedrooms', 'bathrooms', 'price', 'number_of_reviews', 'reviews_per_month'], 
+             y_vars = ['bedrooms', 'bathrooms', 'price', 'number_of_reviews', 'reviews_per_month'])
 g.map_lower(corrfunc)
+g.savefig('.\Images\pairplot_2.jpg', dpi=600)
 
 
 
